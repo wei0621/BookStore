61e8828d8274d876ef4f37de6224974d8bd052a1
wei0621 <524367200@qq.com>
2012-07-10 15:06:18 +0800

    add bookstore

---
+++ bookstore/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bookstore::Application.config.secret_token = '7ef9fa9244e7a06e94743f9c0a533a4288002d393e8a2bf2f43f876e68fbbdc6a7f7faffd6ad0c4701ffced9fb24faeec46daa4676b79608ec357d2f70d07fed'
